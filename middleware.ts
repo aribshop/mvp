e5820df8c5a39b0ad04536b01b08c402c4369ba7
nabildroid <pni20156789@gmail.com>
2022-10-30 14:58:14 +0100

    not rewriting the router for localhost

--- middleware.ts
+++ middleware.ts
@@ -8,12 +8,12 @@ export default function middleware(request: Request) {
   const url = new URL(request.url);
 
   // check if the url is Next specific and rewrite it
-  if (url.pathname.startsWith("/_next")) {
+  const subdomain = url.hostname.split(".")[0];
+
+  if (url.pathname.startsWith("/_next") || subdomain == "localhost") {
     return rewrite(url);
   }
 
-  // extract the subdomain from the request
-  const subdomain = url.hostname.split(".")[0];
   // prepend the subdomain to the request url
   url.pathname = `/website/${subdomain}${url.pathname}`;
 
