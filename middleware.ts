13d3605da9e196c533837648ec22c2caa08f6787
nabildroid <pni20156789@gmail.com>
2022-12-23 19:05:10 +0100

    get template from redis and save it in the cookie
    in this way we don't have to fetch the template each time
    the cookie contain the template

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { rewrite } from "@vercel/edge";
-import { hostname } from "os";
+
+import { Redis } from "@upstash/redis";
 
 export const config = {
   // Only run the middleware on the home route
 };
 
-export default function middleware(request: Request) {
+export default async function middleware(request: Request) {
   const url = new URL(request.url);
 
   // check if the url is Next specific and rewrite it
@@ -13,18 +14,55 @@ export default function middleware(request: Request) {
 
   console.log("CC" + url.hostname);
 
-  if (subdomain === "dashboard") {
-    url.pathname = `/dashboard/${url.pathname}`;
-  }
+  // get template from cookie
+  const cookie = request.headers.get("cookie");
+  let template = "default";
 
-  if (url.pathname.startsWith("/_next") || subdomain == "localhost") {
+  if (url.pathname.startsWith("/_next")) {
     return rewrite(url);
   }
 
+  if (cookie) {
+    const cookies = cookie.split(";");
+    const cookieTemplate = cookies.find((c) =>
+      c.trim().startsWith("template=")
+    );
+    if (cookieTemplate) {
+      template = cookieTemplate.split("=")[1];
+    }
+  }
+  console.log(template);
+
+  if (template == "default") {
+    console.time("redis");
+    const redis = new Redis({
+      url: process.env.REDIS_URL as string,
+      token: process.env.REDIS_TOKEN as string,
+    });
+
+    template =
+      ((await redis.hget("templates", subdomain)) as string) ?? "default";
+    console.timeEnd("redis");
+  }
+
+  if (subdomain == "localhost") {
+    url.searchParams.append("t", template);
+    return rewrite(url, {
+      headers: {
+        "set-cookie": `template=${template}; path=/; max-age=31536000; HttpOnly`,
+      },
+    });
+  }
+
   // prepend the subdomain to the request url
-  url.pathname = `/website/${subdomain}${url.pathname}`;
+  // todo add search params
+  url.pathname = `/website/${subdomain}${url.pathname}?&t=${template}`;
 
   console.log(url.pathname);
   // return the modified request
-  return rewrite(url);
+  return rewrite(url, {
+    headers: {
+      "set-cookie": `template=${template}; path=/; max-age=31536000; HttpOnly`,
+    },
+  });
 }
