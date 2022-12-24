412d01351849f054ec5587856f2b47aa5661dcb2
nabildroid <pni20156789@gmail.com>
2022-12-24 18:56:06 +0100

    mesuring latency of the API

--- app/(websites)/website/[subname]/templates/landing/utils/backdata.ts
+++ app/(websites)/website/[subname]/templates/landing/utils/backdata.ts
@@ -3,7 +3,9 @@ const API = (location: string) => {
 };
 
 export async function getSite(subname: string) {
+  console.time("ARIB-API:getSite")
   const response = await fetch(API("/site/" + subname));
+  console.timeEnd("ARIB-API:getSite")
   const data = await response.json();
 
   return data.site;
